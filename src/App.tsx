a65260b705b3daa87c3a5acaa1ed22bf00ffdca9
Chartine02 <noellachartine125@gmail.com>
2024-12-17 15:58:58 +0200

    fix: remove tailwindcss

--- src/App.tsx
+++ src/App.tsx
@@ -1,11 +1,7 @@
 import { Button } from "./components/ui/button";
 
 function App() {
-  return (
-    <Button colorPalette="blue" variant="subtle">
-      Click Me!
-    </Button>
-  );
+  return <Button>Click Me!</Button>;
 }
 
 export default App;
