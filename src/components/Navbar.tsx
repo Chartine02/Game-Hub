027d72811d9a8748a8127682e0ba1df8e967911a
Chartine02 <noellachartine125@gmail.com>
2024-12-19 11:28:38 +0200

    feat: toggle light and dark mode

--- src/components/Navbar.tsx
+++ src/components/Navbar.tsx
@@ -1,10 +1,12 @@
 import { HStack, Image, Text } from "@chakra-ui/react";
 import logo from "../assets/logo.webp";
+import { ColorModeButton } from "./ui/color-mode";
 
 const Navbar = () => {
   return (
     <HStack>
       <Image src={logo} boxSize="60px" />
+      <ColorModeButton />
       <Text>NavBar</Text>
     </HStack>
   );
