dcbebf9f85499e0efdf80990ba55e51fc1d5fb64
Chartine02 <noellachartine125@gmail.com>
2024-12-20 16:36:48 +0200

    feat: build color mode switch

--- src/components/Navbar.tsx
+++ src/components/Navbar.tsx
@@ -1,13 +1,12 @@
-import { HStack, Image, Text } from "@chakra-ui/react";
+import { HStack, Image } from "@chakra-ui/react";
 import logo from "../assets/logo.webp";
 import { ColorModeButton } from "./ui/color-mode";
 
 const Navbar = () => {
   return (
-    <HStack>
+    <HStack justifyContent={"space-between"} padding="10px">
       <Image src={logo} boxSize="60px" />
       <ColorModeButton />
-      <Text>NavBar</Text>
     </HStack>
   );
 };
