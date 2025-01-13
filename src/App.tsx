3e29da24fce3323513519a14c16cc50700190fb1
Chartine02 <noellachartine125@gmail.com>
2025-01-13 17:34:51 +0200

    feat: create dynamic heading

--- src/App.tsx
+++ src/App.tsx
@@ -7,6 +7,7 @@ import { useState } from "react";
 import PlatformSelector from "./components/PlatformSelector";
 import { Platform } from "./hooks/useGames";
 import SortSelector from "./components/SortSelector";
+import GameHeading from "./components/GameHeading";
 
 export interface GameQuery {
   genre: Genre | null;
@@ -41,6 +42,7 @@ function App() {
         />
       </Stack>
       <GridItem spaceY={3} area="main">
+        <GameHeading gameQuery={gameQuery} />
         <HStack spaceX={3} marginBottom={5}>
           <PlatformSelector
             selectedPlatform={gameQuery.platform}
